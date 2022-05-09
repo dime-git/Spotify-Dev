1ecd44dad7603f2a9986870a0fb58b34e65067f5
Dimitar <dimemilkov1@gmail.com>
2022-05-09 15:09:51 +0200

    removal

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Components/User/login';
+import Header from './Components/User/header';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div id="body">
       <Container>
         <BrowserRouter>
+        <Header/>
           <Routes>
             <Route path='/login' element={<Login />} />
           </Routes>
