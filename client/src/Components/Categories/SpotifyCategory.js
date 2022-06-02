8aa191af6d07d7aafb4a9d21fb13659849182d2c
dime-git <dimemilkov1@gmail.com>
2022-06-02 15:08:03 +0200

    Listing categories on homepage

--- client/src/Components/Categories/SpotifyCategory.js
+++ client/src/Components/Categories/SpotifyCategory.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import '../assets/SpotifyCategory.css';
 
 const SpotifyCategory = ({ imgUrl, categoryName }) => {
-    <div className='row'>
-        <div className='col-4'>
-            <div className='card'>
-                <img src={imgUrl} alt='alt_text' />
-                <div className='card-body'>
-                    <h5 className='card-title'>{categoryName}</h5>
-                </div>
-            </div>
-        </div>
-    </div>
+    return (
+            <Card className="card-container">
+                <Card.Img className="image-container" variant="top" src={imgUrl} />
+                <Card.Body className="card-content">
+                    <Card.Title className="card-title">{categoryName}</Card.Title>
+                    <Card.Text className="card-text">
+                        All you need to know about the music you like.
+                    </Card.Text>
+                    <Button className="btn" variant="primary">{categoryName}</Button>
+                </Card.Body>
+            </Card>
+    )
 }
 
 export default SpotifyCategory;
--- client/src/Pages/homePage.js
+++ client/src/Pages/homePage.js
@@ -6,17 +6,16 @@ import SpotifyCategory from '../Components/Categories/SpotifyCategory';
 function Homepage() {
     const dispatch = useDispatch();
 
-    const { spotify: categories } = useSelector(state => state)
+    const { spotify: { categories } } = useSelector(state => state)
 
     useEffect(() => {
         dispatch(getCategories())
     }, [])
 
-    
     return (
         <div className="container">
-            {categories?.categories?.items !== undefined && categories?.categories?.items.map((item, idx) => {
-                return <SpotifyCategory key={idx} imgUrl={item.icons[0].url} categoryName={item.name} />
+            {categories?.categories?.categories?.items !== undefined && categories?.categories?.categories?.items.map((item, idx) => {
+                return <SpotifyCategory key={idx} imgUrl={item?.icons[0]?.url} categoryName={item?.name} />
             })}
         </div>
     )
